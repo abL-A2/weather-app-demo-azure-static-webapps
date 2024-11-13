d7a002a285db241c3e1bcf7ce3a585400047e688
abL-A2 <amiela@missionreadyhq.com>
2024-11-13 18:44:18 +1300

    added my API key from weatherapi

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const cityInput = document.getElementById("input");
 const dispalySection = document.getElementById("display");
 
-
-const API_KEY = "650ad8fb3c6e4ecab3190253232706";
+const API_KEY = "810b8d7affb541199bd211255242509";
 
 const handleClick = () => {
   let currentCity = cityInput.value;
 
   const getWeather = fetch(
-    `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${
-      currentCity || "London"
-    }&aqi=no`
+    `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${currentCity || "London"}&aqi=no`
   )
     .then((res) => res.json())
     .then((data) => {
-        dispalySection.innerHTML =
-      ` <p>${data.location.name}'s temperature is ${data.current.temp_c}℃.</p>
+      dispalySection.innerHTML = ` <p>${data.location.name}'s temperature is ${data.current.temp_c}℃.</p>
         <p>It's ${data.current.condition.text}.</p>
         <img src="${data.current.condition.icon}" alt="icon"/>
       `;
